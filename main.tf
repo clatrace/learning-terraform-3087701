23c3dc2ce2503d7bc541b1c3b50c3c1ab9f12aed
clatrace <120337030+clatrace@users.noreply.github.com>
2022-12-12 08:58:26 -0800

    change to blog_vpc for module name

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "blog_sg" {
   version = "4.16.2"
   name = "blog"
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
